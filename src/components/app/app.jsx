71c092802655ffde7dcac442f5fc6e05e26ae4e6
qaSokolovaAG <nast1ageorg@gmail.com>
2022-04-26 21:04:04 +0400

    add lecture 2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { AppRoute } from "../../const.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AppRoute } from "../../const";
 
-import Main from "../../pages/main/main"
+import Main from "../../pages/main/main";
 import Archive from "../../pages/archive/archive";
-import Form from "../../pages/form/form"
+import Form from "../../pages/form/form";
 
 
 const App = () => {
     return(
-        //<Main mode={AppRoute.MAIN} />
-        //<Archive mode={AppRoute.ARCHIVE} />
-        <Form mode={AppRoute.EVENT}/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={AppRoute.MAIN} component={Main} />
+                <Route exact path={AppRoute.ARCHIVE} component={Archive} />
+                <Route exact path={AppRoute.EVENT /*Редактирование*/} component={Form} />
+                <Route exact path={AppRoute.ADD /*Добавление*/} component={Form} />
+            </Switch>
+        </BrowserRouter>
     );
 };
 
